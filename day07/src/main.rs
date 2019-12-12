c6247401c311ef2c7bc01ba2825ec562d0ec8a14
Philip Daniels <Philip.Daniels1971@gmail.com>
2019-12-12 20:53:09 +0000

    Start day 7

---
+++ day07/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // First amplifier input is 0.
+    // There are 5 amplifiers wired in series.
+
+    // Phase setting is an integer 0..=4.
+    // Each amp will ask for this setting.
+    // Each is used only once (what are all the possible
+    // permutations of 0..=4 ?)
+
+
+}
