8cec6c2b0893b449f9fa9efcd5bd4cbcf47aafb5
Philip Daniels <Philip.Daniels1971@gmail.com>
2019-12-15 15:33:25 +0000

    Generate phase setting permutations.

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,4 +1,22 @@
+use permutohedron::LexicalPermutation;
+
+fn get_phase_setting_permutations() -> Vec<Vec<i32>> {
+    let mut phase_settings = vec![0, 1, 2, 3, 4];
+    let mut permutations = Vec::new();
+    loop {
+        permutations.push(phase_settings.clone());
+        if !phase_settings.next_permutation() {
+            break;
+        }
+    }
+
+    permutations
+}
+
 fn main() {
+    let permutations = get_phase_setting_permutations();
+    println!("There are {} permutations.", permutations.len());
+
     // First amplifier input is 0.
     // There are 5 amplifiers wired in series.
 
