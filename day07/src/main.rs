cc181b26edc07e3967294ed91a1a2e29a2c8a808
Philip Daniels <Philip.Daniels1971@gmail.com>
2019-12-15 16:05:43 +0000

    WIP

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,4 +1,5 @@
 use permutohedron::LexicalPermutation;
+use computer::Computer;
 
 fn get_phase_setting_permutations() -> Vec<Vec<i32>> {
     let mut phase_settings = vec![0, 1, 2, 3, 4];
@@ -15,7 +16,7 @@ fn get_phase_setting_permutations() -> Vec<Vec<i32>> {
 
 fn get_input(raw_input: &str) -> Vec<i32> {
     raw_input.split(',')
-        .map(|s| i32::parse(s).expect("Input should be an integer"))
+        .map(|s| s.parse::<i32>().expect("Input should be an integer"))
         .collect()
 }
 
@@ -24,9 +25,25 @@ fn get_puzzle_input() -> Vec<i32> {
     get_input(data)
 }
 
+fn make_amplifier() -> Computer {
+    let program = get_puzzle_input();
+    Computer::load_program(program)
+}
+
 fn main() {
     let permutations = get_phase_setting_permutations();
     println!("There are {} permutations.", permutations.len());
+
+    // TODO: Run all these in parallel!
+    let mut max_output_signal = 0;
+    for permutation in permutations {
+        let output_signal = calculate_output_signal(&permutation);
+        if output_signal > max_output_signal {
+            max_output_signal = output_signal;
+            println!("New max_output_signal of {} found for phase settings {:?}",
+                max_output_signal, permutation);
+        }
+    }
 
     // First amplifier input is 0.
     // There are 5 amplifiers wired in series.
@@ -35,6 +52,16 @@ fn main() {
     // Each amp will ask for this setting.
     // Each is used only once (what are all the possible
     // permutations of 0..=4 ?)
+}
 
+fn calculate_output_signal(permutation: &[i32]) -> i32 {
+    let mut amp_a = make_amplifier();
 
+    /// The computer currently returns 'output' that is simply
+    /// the value left at address 0. This is UTTERLY IRRELEVANT
+    /// for this problem, where the output we want is the value
+    /// that is written to stdout.
+    let ignored_output = amp_a.run().expect("Program should produce a valid output");
+
+    0
 }
