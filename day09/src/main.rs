3e3a745fd4589c1e0aae2f0901f8a7f3754cb77e
Philip Daniels <Philip.Daniels1971@gmail.com>
2019-12-17 21:19:47 +0000

    Start day09.

---
+++ day09/src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    let input = get_puzzle_input();
+}
+
+fn get_input(raw_input: &str) -> Vec<i64> {
+    raw_input.split(',')
+        .map(|s| s.parse::<i64>().expect("Input should be an integer"))
+        .collect()
+}
+
+fn get_puzzle_input() -> Vec<i64> {
+    let data = include_str!("input.txt");
+    get_input(data)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn test1() {
+        let input = get_input("109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99");
+    }
+
+    #[test]
+    pub fn test2() {
+        let input = get_input("1102,34915192,34915192,7,4,7,99,0");
+    }
+
+    #[test]
+    pub fn test3() {
+        let input = get_input("104,1125899906842624,99");
+    }
+}
